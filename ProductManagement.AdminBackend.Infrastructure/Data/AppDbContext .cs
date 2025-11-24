4dec67d97452a65cede0ca93e93a9f02238bd6ce
Bonolo Selowa <bonololloyd003@gmail.com>
2025-11-24 18:30:16 +0200

    added DbContext

---
+++ ProductManagement.AdminBackend.Infrastructure/Data/AppDbContext .cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.AdminBackend.Core.Entities;
+
+namespace ProductManagement.AdminBackend.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Admin> Admins => Set<Admin>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<Customer> Customers => Set<Customer>();
+    }
+}
